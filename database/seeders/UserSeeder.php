4d7dbb3f7100ac6d0013eb71be0346ca65bc8a0d
absandar <abrahamsandoval1605@gmail.com>
2026-02-05 16:53:37 -0600

    add seeder and mod user model

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = \App\Models\User::create([
+            'name' => 'Sandar',
+            'username' => 'admin',
+            'email' => 'admin@admin.local',
+            'password' => bcrypt('password'),
+        ]);
+    }
+}
